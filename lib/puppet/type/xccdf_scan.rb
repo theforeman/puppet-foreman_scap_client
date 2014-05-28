0259ac8aae290014fd25d1e8f7c6e31d43c86144
Šimon Lukašík <isimluk@fedoraproject.org>
2014-05-28 10:47:27 -0400

    Introduce new puppet type: xccdf_scan

---
+++ lib/puppet/type/xccdf_scan.rb
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+Puppet::Type.newtype(:xccdf_scan) do
+  @doc = "Creates an SCAP report directory with SCAP result files"
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc "Name of the SCAP report"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+end
