dd6bd55230f5371778680301f3a36cc779c6f9ec
Shlomi Zadok <shlomi@ben-hanna.com>
2015-04-14 15:52:31 +0300

    fix wrong fact name for ca_file

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class foreman_scap_client::params {
-  $ca_file          = pick($::rh_certificate_consumer_ca_file, '/var/lib/puppet/ssl/certs/ca.pem')
+  $ca_file          = pick($::rh_certificate_repo_ca_file, '/var/lib/puppet/ssl/certs/ca.pem')
   $host_certificate = pick($::rh_certificate_consumer_host_cert, "/var/lib/puppet/ssl/certs/${fqdn}.pem")
   $host_private_key = pick($::rh_certificate_consumer_host_key, "/var/lib/puppet/ssl/private_keys/${fqdn}.pem")
 }
