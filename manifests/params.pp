0334daef55698e9b8d8cb889e9e3d81c03c0778b
Ali <ali.allomani@gmail.com>
2016-03-21 01:01:26 +0300

    fix for invalid cert path when host is set in upper-case letters

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,6 @@
 class foreman_scap_client::params {
+  $fqdn_s = downcase($fqdn)
   $ca_file          = pick($::rh_certificate_repo_ca_file, '/var/lib/puppet/ssl/certs/ca.pem')
-  $host_certificate = pick($::rh_certificate_consumer_host_cert, "/var/lib/puppet/ssl/certs/${fqdn}.pem")
-  $host_private_key = pick($::rh_certificate_consumer_host_key, "/var/lib/puppet/ssl/private_keys/${fqdn}.pem")
+  $host_certificate = pick($::rh_certificate_consumer_host_cert, "/var/lib/puppet/ssl/certs/${fqdn_s}.pem")
+  $host_private_key = pick($::rh_certificate_consumer_host_key, "/var/lib/puppet/ssl/private_keys/${fqdn_s}.pem")
 }
