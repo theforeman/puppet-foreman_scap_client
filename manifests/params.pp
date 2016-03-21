cc5e41ef6f85512c3121c15b3bd97fa7cd555999
Ali <ali.allomani@gmail.com>
2016-03-22 01:11:59 +0300

    downcased variable name changed from $fqdn_s to $downcased_fqd, EOF added

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class foreman_scap_client::params {
-  $fqdn_s = downcase($fqdn)
+  $downcased_fqdn = downcase($fqdn)
   $ca_file          = pick($::rh_certificate_repo_ca_file, '/var/lib/puppet/ssl/certs/ca.pem')
-  $host_certificate = pick($::rh_certificate_consumer_host_cert, "/var/lib/puppet/ssl/certs/${fqdn_s}.pem")
-  $host_private_key = pick($::rh_certificate_consumer_host_key, "/var/lib/puppet/ssl/private_keys/${fqdn_s}.pem")
+  $host_certificate = pick($::rh_certificate_consumer_host_cert, "/var/lib/puppet/ssl/certs/${$downcased_fqdn}.pem")
+  $host_private_key = pick($::rh_certificate_consumer_host_key, "/var/lib/puppet/ssl/private_keys/${$downcased_fqdn}.pem")
 }
