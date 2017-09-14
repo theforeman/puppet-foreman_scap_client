f28ddfdca639b400cc4cc51fd3e585b93ed85cac
James Weaver <james.weaver.e6y0@statefarm.com>
2017-09-14 15:23:03 -0500

    Moved ensure to first line of definition for 'foreman_scap_client_cron' and 'foreman_scap_client' files, per Puppet Style Guide 9.3
    Removed extra space in front of 'default' option in osfamily selection
    
    Switched to single quotes for hardcoded 'path' option in cron removal
    section, per Puppet Style Guide 6
    
    Added comment to params.pp, loosely per Puppet Style Guide 7.1
    
    Fixed variable scoping for $fqdn in params.pp, per Puppet Style Guide 13.2
    
    Modified 'rh_certificate_*' variables in params.pp to use facts hash. This
    allows the module to be compatible with Puppet's strict vars option.
    
    Mofified metadata.json fix forge warnings.
    - Converted license to SPDX string
    - Added os_support key (may need expanded)
    - Added upper bound on dependencies

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,13 @@
+# Params class for parent foreman_scap_client
 class foreman_scap_client::params {
-  $downcased_fqdn = downcase($fqdn)
+  $downcased_fqdn = downcase($::fqdn)
   if versioncmp('4.0.0', $clientversion) > 0 {
     $ssl_dir = '/var/lib/puppet/ssl'
   } else {
     $ssl_dir = '/etc/puppetlabs/puppet/ssl'
   }
 
-  $ca_file          = pick($::rh_certificate_repo_ca_file, "${ssl_dir}/certs/ca.pem")
-  $host_certificate = pick($::rh_certificate_consumer_host_cert, "${ssl_dir}/certs/${$downcased_fqdn}.pem")
-  $host_private_key = pick($::rh_certificate_consumer_host_key, "${ssl_dir}/private_keys/${$downcased_fqdn}.pem")
+  $ca_file          = pick($facts['rh_certificate_repo_ca_file'], "${ssl_dir}/certs/ca.pem")
+  $host_certificate = pick($facts['rh_certificate_consumer_host_cert'], "${ssl_dir}/certs/${$downcased_fqdn}.pem")
+  $host_private_key = pick($facts['rh_certificate_consumer_host_key'], "${ssl_dir}/private_keys/${$downcased_fqdn}.pem")
 }
