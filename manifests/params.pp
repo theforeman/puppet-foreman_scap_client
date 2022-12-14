5975a1fd5c1170462b7d2b0690faf2dd8bee82d9
Evgeni Golov <evgeni@golov.de>
2022-12-14 12:08:14 +0100

    simplify cert params

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,36 +3,12 @@
 class foreman_scap_client::params {
   $ssl_dir = '/etc/puppetlabs/puppet/ssl'
 
-  # Set CA file
-  if defined('$facts[rh_certificate_repo_ca_file]') {
-    $rh_certificate_repo_ca_file = $facts['rh_certificate_repo_ca_file']
-  }
-  else {
-    $rh_certificate_repo_ca_file = undef
-  }
-
-  # Set host cert
-  if defined('$facts[rh_certificate_consumer_host_cert]') {
-    $rh_certificate_consumer_host_cert = $facts['rh_certificate_consumer_host_cert']
-  }
-  else {
-    $rh_certificate_consumer_host_cert = undef
-  }
-
-  # Set private key
-  if defined('$facts[rh_certificate_consumer_host_key]') {
-    $rh_certificate_consumer_host_key = $facts['rh_certificate_consumer_host_key']
-  }
-  else {
-    $rh_certificate_consumer_host_key = undef
-  }
-
   $package_name = $facts['os']['family'] ? {
     'Debian' => 'ruby-foreman-scap-client',
     default  => 'rubygem-foreman_scap_client'
   }
 
-  $ca_file          = pick($rh_certificate_repo_ca_file, "${ssl_dir}/certs/ca.pem")
-  $host_certificate = pick($rh_certificate_consumer_host_cert, "${ssl_dir}/certs/${trusted['certname']}.pem")
-  $host_private_key = pick($rh_certificate_consumer_host_key, "${ssl_dir}/private_keys/${trusted['certname']}.pem")
+  $ca_file          = pick($facts['rh_certificate_repo_ca_file'], "${ssl_dir}/certs/ca.pem")
+  $host_certificate = pick($facts['rh_certificate_consumer_host_cert'], "${ssl_dir}/certs/${trusted['certname']}.pem")
+  $host_private_key = pick($facts['rh_certificate_consumer_host_key'], "${ssl_dir}/private_keys/${trusted['certname']}.pem")
 }
